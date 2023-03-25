206dc90e9364d4602ba0c35c4f715f32e52b81a7
MaksLisoviy <128755236+MaksLisoviy@users.noreply.github.com>
2023-03-26 00:17:58 +0200

    Responsive real estate landing page

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const swiper = new Swiper('.slider-main-block', {
+	// Optional parameters
+	loop: true,
+	// Navigation arrows
+	navigation: {
+		nextEl: '.body-main-block__arrow.swiper-button-next',
+		prevEl: '.body-main-block__arrow.swiper-button-prev',
+	},
+});
+
+// Таби
+const tabNavItems = document.querySelectorAll('.tabs-deals__button');
+const tabItems = document.querySelectorAll('.item-tabs');
+document.addEventListener("click", function (e) {
+	const targetElement = e.target;
+	let currentActiveIndex = null;
+	let newActiveIndex = null;
+	if (targetElement.closest('.tabs-deals__button')) {
+		tabNavItems.forEach((tabNavItem, index) => {
+			if (tabNavItem.classList.contains('active')) {
+				currentActiveIndex = index;
+				tabNavItem.classList.remove('active');
+			}
+			if (tabNavItem === targetElement) {
+				newActiveIndex = index;
+			}
+		});
+		targetElement.classList.add('active');
+		tabItems[currentActiveIndex].classList.remove('active');
+		tabItems[newActiveIndex].classList.add('active');
+	}
+});
+
+
+
+const iconMenu = document.querySelector('.menu__icon')
+if (iconMenu) {
+	const menuBody = document.querySelector('.menu__body')
+	iconMenu.addEventListener("click", function (e) {
+		document.body.classList.toggle('lock');
+		iconMenu.classList.toggle('active');
+		menuBody.classList.toggle('active');
+	})
+}
